fb6c19edde0f3850c51bfc1493cecddb9440c5c1
Ludwig Stecher <ludwig.stecher@gmx.de>
2020-12-05 22:19:16 +0100

    Remove unnecessary struct

--- 2020/src/day5.rs
+++ 2020/src/day5.rs
@@ -1,42 +1,30 @@
-struct BoardingPass {
-    row: u8,
-    col: u8,
-}
-
-impl BoardingPass {
-    fn id(&self) -> usize {
-        (self.row as usize * 8) + self.col as usize
-    }
-}
-
 #[aoc_generator(day5)]
-fn input_generator(input: &str) -> Vec<BoardingPass> {
+fn input_generator(input: &str) -> Vec<usize> {
     input.lines().map(make_boarding_pass).collect()
 }
 
-fn make_boarding_pass(input: &str) -> BoardingPass {
+fn make_boarding_pass(input: &str) -> usize {
     let row = input[0..7]
         .bytes()
-        .fold(0, |acc, b| (acc << 1) + if b == b'F' { 0 } else { 1 });
+        .fold(0, |acc, b| (acc << 1) + (b == b'B') as usize);
     let col = input[7..10]
         .bytes()
-        .fold(0, |acc, b| (acc << 1) + if b == b'L' { 0 } else { 1 });
-    BoardingPass { row, col }
+        .fold(0, |acc, b| (acc << 1) + (b == b'R') as usize);
+    (row * 8) + col
 }
 
 #[aoc(day5, part1)]
-fn part1(values: &[BoardingPass]) -> usize {
-    values.iter().map(BoardingPass::id).max().unwrap()
+fn part1(passes: &[usize]) -> usize {
+    passes.iter().copied().max().unwrap()
 }
 
 #[aoc(day5, part2)]
-fn part2(values: &[BoardingPass]) -> usize {
+fn part2(passes: &[usize]) -> usize {
     // enough memory to fit 883 bits
     let mut bitfield = [0u32; 28];
 
-    for value in values {
-        let id = value.id();
-        bitfield[id / 32] |= 0x8000_0000 >> (id % 32);
+    for &pass in passes {
+        bitfield[pass / 32] |= 0x8000_0000 >> (pass % 32);
     }
 
     let (i, n) = bitfield
